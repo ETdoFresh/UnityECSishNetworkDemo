49373b2eb794b6e3892e6e099fe8baca7f3624c3
ETdoFresh <etdofresh@gmail.com>
2019-11-26 17:33:04 -0600

    Update ECSish MonoBehaviourSystem

--- Assets/Plugins/ECSish/MonoBehaviourSystem.cs
+++ Assets/Plugins/ECSish/MonoBehaviourSystem.cs
@@ -7,13 +7,6 @@ namespace ECSish
 {
     public class MonoBehaviourSystem : MonoBehaviour
     {
-        private bool isGlobalSystem;
-
-        private void Awake()
-        {
-            isGlobalSystem = GetComponent<GlobalSystem>();
-        }
-
         public IEnumerable<Tuple<T1>> GetEntities<T1>()
             where T1 : MonoBehaviourComponentData
         {
@@ -21,7 +14,7 @@ namespace ECSish
             {
                 if (!component1) continue;
                 if (!component1.enabled) continue;
-                if (!isGlobalSystem && component1.gameObject.scene.handle != gameObject.scene.handle) continue;
+                if (component1.gameObject.scene.handle != gameObject.scene.handle) continue;
                 yield return new Tuple<T1>((T1)component1);
             }
         }
@@ -34,7 +27,7 @@ namespace ECSish
             {
                 if (!component1) continue;
                 if (!component1.enabled) continue;
-                if (!isGlobalSystem && component1.gameObject.scene.handle != gameObject.scene.handle) continue;
+                if (component1.gameObject.scene.handle != gameObject.scene.handle) continue;
                 foreach (var component2 in Entity.GetComponents<T2>())
                 {
                     if (!component2) continue;
@@ -54,7 +47,7 @@ namespace ECSish
             {
                 if (!component1) continue;
                 if (!component1.enabled) continue;
-                if (!isGlobalSystem && component1.gameObject.scene.handle != gameObject.scene.handle) continue;
+                if (component1.gameObject.scene.handle != gameObject.scene.handle) continue;
                 foreach (var component2 in Entity.GetComponents<T2>())
                 {
                     if (!component2) continue;
@@ -81,7 +74,93 @@ namespace ECSish
             {
                 if (!component1) continue;
                 if (!component1.enabled) continue;
-                if (!isGlobalSystem && component1.gameObject.scene.handle != gameObject.scene.handle) continue;
+                if (component1.gameObject.scene.handle != gameObject.scene.handle) continue;
+                foreach (var component2 in Entity.GetComponents<T2>())
+                {
+                    if (!component2) continue;
+                    if (!component2.enabled) continue;
+                    if (component2.gameObject != component1.gameObject) continue;
+                    foreach (var component3 in Entity.GetComponents<T3>())
+                    {
+                        if (!component3) continue;
+                        if (!component3.enabled) continue;
+                        if (component3.gameObject != component2.gameObject) continue;
+                        foreach (var component4 in Entity.GetComponents<T4>())
+                        {
+                            if (!component4) continue;
+                            if (!component4.enabled) continue;
+                            if (component4.gameObject != component3.gameObject) continue;
+                            yield return new Tuple<T1, T2, T3, T4>((T1)component1, (T2)component2, (T3)component3, (T4)component4);
+                        }
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Tuple<T1>> GetAllEntities<T1>()
+            where T1 : MonoBehaviourComponentData
+        {
+            foreach (var component1 in Entity.GetComponents<T1>())
+            {
+                if (!component1) continue;
+                if (!component1.enabled) continue;
+                yield return new Tuple<T1>((T1)component1);
+            }
+        }
+
+        public IEnumerable<Tuple<T1, T2>> GetAllEntities<T1, T2>()
+            where T1 : MonoBehaviourComponentData
+            where T2 : MonoBehaviourComponentData
+        {
+            foreach (var component1 in Entity.GetComponents<T1>())
+            {
+                if (!component1) continue;
+                if (!component1.enabled) continue;
+                foreach (var component2 in Entity.GetComponents<T2>())
+                {
+                    if (!component2) continue;
+                    if (!component2.enabled) continue;
+                    if (component2.gameObject != component1.gameObject) continue;
+                    yield return new Tuple<T1, T2>((T1)component1, (T2)component2);
+                }
+            }
+        }
+
+        public IEnumerable<Tuple<T1, T2, T3>> GetAllEntities<T1, T2, T3>()
+            where T1 : MonoBehaviourComponentData
+            where T2 : MonoBehaviourComponentData
+            where T3 : MonoBehaviourComponentData
+        {
+            foreach (var component1 in Entity.GetComponents<T1>())
+            {
+                if (!component1) continue;
+                if (!component1.enabled) continue;
+                foreach (var component2 in Entity.GetComponents<T2>())
+                {
+                    if (!component2) continue;
+                    if (!component2.enabled) continue;
+                    if (component2.gameObject != component1.gameObject) continue;
+                    foreach (var component3 in Entity.GetComponents<T3>())
+                    {
+                        if (!component3) continue;
+                        if (!component3.enabled) continue;
+                        if (component3.gameObject != component2.gameObject) continue;
+                        yield return new Tuple<T1, T2, T3>((T1)component1, (T2)component2, (T3)component3);
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Tuple<T1, T2, T3, T4>> GetAllEntities<T1, T2, T3, T4>()
+            where T1 : MonoBehaviourComponentData
+            where T2 : MonoBehaviourComponentData
+            where T3 : MonoBehaviourComponentData
+            where T4 : MonoBehaviourComponentData
+        {
+            foreach (var component1 in Entity.GetComponents<T1>())
+            {
+                if (!component1) continue;
+                if (!component1.enabled) continue;
                 foreach (var component2 in Entity.GetComponents<T2>())
                 {
                     if (!component2) continue;
