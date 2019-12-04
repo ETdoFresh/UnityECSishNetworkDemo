c5be77400e816aaa7722ababacb12d22d2ef156e
ETdoFresh <ETdoFresh@gmail.com>
2019-12-04 07:41:14 -0600

    Get Application Information

---
+++ Assets/Network Tutorial/00_UISetup/ApplicationInformation.cs
@@ -0,0 +1,13 @@
+﻿using ECSish;
+
+public class ApplicationInformation : MonoBehaviourComponentData
+{
+    public float fps;
+    public string platform;
+    public string version;
+    public string companyName;
+    public string productName;
+    public string unityVersion;
+    public int targetFrameRate;
+    public int vSyncCount = -1;
+}
---
+++ Assets/Network Tutorial/00_UISetup/GetApplicationInformationSystem.cs
@@ -0,0 +1,38 @@
+﻿using ECSish;
+using UnityEngine;
+
+public class GetApplicationInformationSystem : MonoBehaviourSystem
+{
+    private void Update()
+    {
+        foreach (var entity in GetEntities<ApplicationInformation>())
+        {
+            entity.Item1.fps = Time.timeScale / Time.deltaTime;
+
+            if (string.IsNullOrEmpty(entity.Item1.platform))
+                entity.Item1.platform = Application.platform.ToString();
+
+            if (string.IsNullOrEmpty(entity.Item1.version))
+                entity.Item1.version = Application.version;
+
+            if (string.IsNullOrEmpty(entity.Item1.companyName))
+                entity.Item1.companyName = Application.companyName;
+
+            if (string.IsNullOrEmpty(entity.Item1.productName))
+                entity.Item1.productName = Application.productName;
+
+            if (string.IsNullOrEmpty(entity.Item1.unityVersion))
+                entity.Item1.unityVersion = Application.unityVersion;
+
+            if (entity.Item1.targetFrameRate == 0 && Application.targetFrameRate != 0)
+                entity.Item1.targetFrameRate = Application.targetFrameRate;
+            else if (entity.Item1.targetFrameRate != Application.targetFrameRate)
+                Application.targetFrameRate = entity.Item1.targetFrameRate;
+
+            if (entity.Item1.vSyncCount == -1 && QualitySettings.vSyncCount != -1)
+                entity.Item1.vSyncCount = QualitySettings.vSyncCount;
+            else if (entity.Item1.vSyncCount != QualitySettings.vSyncCount)
+                QualitySettings.vSyncCount = entity.Item1.vSyncCount;
+        }
+    }
+}
