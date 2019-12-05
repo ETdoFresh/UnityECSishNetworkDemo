90f7052d3934782bec6f68813498e73eef543f95
ETdoFresh <ETdoFresh@gmail.com>
2019-12-05 08:11:53 -0600

    ClearEventSystem now checks All Entities to see if they are destroyed. (ECSish)
    (cherry picked from commit 7b705464beee4ce56cc3da8ba5987e3c33050877)

--- ClearEventSystem.cs
+++ ClearEventSystem.cs
@@ -6,7 +6,7 @@
         {
             EventSystem.ClearEvents();
 
-            foreach (var entity in GetEntities<EntityDestroyed>())
+            foreach (var entity in GetAllEntities<EntityDestroyed>())
                 if(entity.Item1 && entity.Item1.gameObject)
                     Destroy(entity.Item1.gameObject);
         }
