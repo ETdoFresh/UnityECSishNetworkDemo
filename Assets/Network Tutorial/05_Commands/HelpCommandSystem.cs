23843c7b9a540105f3cfedff3d705b4742670b10
ETdoFresh <ETdoFresh@gmail.com>
2019-12-13 00:13:18 -0600

    Refactor Help, Test, Whisper commands.

--- Assets/Network Tutorial/05_Commands/HelpCommandSystem.cs
+++ Assets/Network Tutorial/05_Commands/HelpCommandSystem.cs
@@ -1,32 +1,30 @@
 ﻿using ECSish;
-using System;
 
 public class HelpCommandSystem : MonoBehaviourSystem
 {
     private void Update()
     {
-//        foreach (var entity in GetEntities<OnReceiveEvent, Session, SocketClientConnection>())
-//        {
-//            var args = entity.Item1.args;
-//            if (args.Length != 2) continue;
+        foreach (var entity in GetEntities<OnReceiveEvent, Session>())
+        {
+            var args = entity.Item1.Args;
+            if (args.Length != 2) continue;
 
-//            var command = args[1].ToLower();
-//            var server = entity.Item2;
-//            if (command == "help" || command == "?")
+            var command = args[1].ToLower();
+            var server = entity.Item2;
+            if (command == "help" || command == "?")
 
-//                EventUtility.CreateOnSendEvent(server.gameObject,
-//$@"{entity.Item1.sessionId} ----------
-//Commands:
-//----------
-//Connect {{IP{{:PORT}}}}
-//Disconnect
-//Exit/Quit
-//Restart
-//Test
-//Chat {{ChatMessage}}
-//Whisper {{ClientIndex}} {{ChatMessage}}
-//Help/?");
-
-//        }
+                ECSEvent.Create<OnSendEvent>(server,
+@"----------
+Commands:
+----------
+Connect {{IP{{:PORT}}}}
+Disconnect
+Exit/Quit
+Restart
+Test
+Chat {{ChatMessage}}
+Whisper {{ClientIndex}} {{ChatMessage}}
+Help/?");
+        }
     }
 }
