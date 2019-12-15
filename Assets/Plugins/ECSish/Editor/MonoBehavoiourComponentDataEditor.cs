79fc4f2c2e4d52e0751e75f86dcbcf6aa18b04ce
ETdoFresh <ETdoFresh@gmail.com>
2019-12-15 14:42:50 -0600

    Start of icon assignment.

---
+++ Assets/Plugins/ECSish/Editor/MonoBehavoiourComponentDataEditor.cs
@@ -0,0 +1,22 @@
+﻿using ECSish;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MonoBehaviourComponentData))]
+public class MonoBehavoiourComponentDataEditor : Editor
+{
+    private void Awake()
+    {
+        MethodInfo SetIconForObject = typeof(EditorGUIUtility).GetMethod("SetIconForObject", BindingFlags.Static | BindingFlags.NonPublic);
+        MethodInfo CopyMonoScriptIconToImporters = typeof(MonoImporter).GetMethod("CopyMonoScriptIconToImporters", BindingFlags.Static | BindingFlags.NonPublic);
+
+        //Monoscript script = whatever;
+        //Texture2D icon = whatever;
+
+        //SetIconForObject.Invoke(null, new[] { script, icon });
+        //CopyMonoScriptToImporters.invoke(null, script);
+    }
+}
