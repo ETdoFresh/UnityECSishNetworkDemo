4321f4cca4bfc749eb699f63ad0c8d58d58c8570
ETdoFresh <ETdoFresh@gmail.com>
2020-01-10 07:24:44 -0600

    Add some notes on a video I saw.

---
+++ Assets/Network Tutorial/QuickNotes/QuickNotes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickNotes : MonoBehaviour
+{
+/*
+I don't know why I'm doing this here, but I am!
+
+    Notes on Networking Video
+    - Looking into Span<T> in C# for memory allocation/less garbage collection
+    - Look into Data Component Arrays instead of Array of Data Components
+        - For example, send Health 1 2 3 Mana 1 2 3 instead of 1 Health Mana 2 Health Mana 3 Health Mana
+    - Mirror? Networking Solution?
+    - UDP libraries (Licren or Enet)
+    - Common to mix HTTP(S) and UDP or Networking
+*/
+}
